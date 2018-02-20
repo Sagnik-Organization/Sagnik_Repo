aaec5cdf3b35131010a0df806711a7281ebc3f92
sd261 <sd261@demo1.sr.kovair.com>
2018-02-20 01:45:04 -0800

    abc

---
+++ Demo_Code/src/Demo_Code_Main.java
@@ -0,0 +1,41 @@
+//Sagnik Project....
+
+class Main
+{
+    static int factorial(int n)
+    {
+        int fact = 1;
+        while (n != 0)
+        {
+            fact = fact*n;
+            n--;
+        }
+        return fact;
+    }
+   
+    static boolean isMain(int n)
+    {
+        int sum = 0;
+ 
+        int temp = n;
+        while (temp != 0)
+        {   
+            sum += factorial(temp%10);
+ 
+            
+            temp = temp/10;
+        }
+ 
+        
+        return (sum == n);
+    }
+
+    public static void main(String[] args)
+    {
+        int n = 145;
+        if (isMain(n))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
